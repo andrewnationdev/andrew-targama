b2e2eeafef6bebac9b67f5bccd0280ee5ac13d43
AndrewNation II <129698289+andrewnationdev@users.noreply.github.com>
2026-01-23 01:42:23 +0000

    correção de prop no card de favoritos

--- app/components/favorites-card.tsx
+++ app/components/favorites-card.tsx
@@ -3,6 +3,6 @@ import { IFavoritesItem } from "../types/types";
 export default function FavoritesCard(item: IFavoritesItem) {
     return <div className="p-4 rounded shadow-md">
         <h3 className="text-lg font-semibold mb-2">{item.translation}</h3>
-        <p className="text-sm text-gray-600">{item.key}</p>
+        <p className="text-sm text-gray-600">{item.text}</p>
     </div>;
 }
